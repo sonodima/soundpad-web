7b5c536f6cace29284ea7f1c90d70ce172f82e4d
sonodima <62671493+sonodima@users.noreply.github.com>
2022-01-23 06:24:41 -0800

    changed button behavior on mobile

--- components/SoundButton.tsx
+++ components/SoundButton.tsx
@@ -8,8 +8,8 @@ const SoundButton = (props: Props) => {
   return (
     <button
       className="flex flex-col items-stretch h-20 w-20 p-2 m-1 text-left rounded-xl overflow-hidden cursor-pointer shadow-xl transition-all bg-gray-600 text-gray-200
-                 hover:scale-95 hover:shadow-lg hover:bg-gray-500 active:scale-100 active:shadow-xl active:bg-gray-700 active:text-gray-100
-                 dark:bg-zinc-800 dark:text-zinc-200 dark:hover:bg-zinc-600 dark:active:bg-zinc-700"
+                 hover:shadow-lg active:scale-95 active:shadow-xl active:bg-gray-700 active:text-gray-100 md:hover:scale-95 md:active:scale-100 md:hover:bg-gray-500 
+                 dark:bg-zinc-800 dark:text-zinc-200 md:dark:hover:bg-zinc-600 dark:active:bg-zinc-700"
       onClick={props.onClick}
     >
       <p className="flex-grow font-medium leading-4 select-none overflow-hidden">
