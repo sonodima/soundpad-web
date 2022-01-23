d850fba09d0bf8dc8e9da9d11d775ebf2c55ae0a
sonodima <62671493+sonodima@users.noreply.github.com>
2022-01-23 07:58:15 -0800

    added pwa support and icons

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
+const withPWA = require("next-pwa");
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+};
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
